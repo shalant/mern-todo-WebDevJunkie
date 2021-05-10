e9255719964c8c1dbf548b2ef57692fe6888c4c2
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-05-09 22:30:13 -0500

    work on server

--- client/server.js
+++ client/server.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = 4000;
 
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+mongoose.connect('mongodb://localhost/todo', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true 
 });
 
-app.listen(port, () => {
-    console.log(`Example app listerning at http://localhost`)
-})
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const user= new User({ username, password });
+    await user.create();
+    res.json({
+        message: 'success'
+    });
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+    })
+    //we're connected!
+});
